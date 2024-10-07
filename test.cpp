1b33eb439112eb6a40a15221fc2260f3713ff793
fabiolalw <158202628+fabiolalw@users.noreply.github.com>
2024-10-07 12:12:48 -0400

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+/*
+Author: Fabiola Li Wu
+Course: CSCI-23500
+Date: 01/31/2024
+
+Creatioin of the Creature class and implementation of it.
+*/#include "Creature.hpp"
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+// main test driver
+int main(){
+    std::cout << "Hello" << endl;
+   return 0;
+}
+
+
